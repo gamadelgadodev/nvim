a09413089ac3dbc9ad564dbe26b6e5db709e5b84
gamadelgado <mdelgado@tecnolika.com>
2023-04-11 15:52:39 -0600

    lspconfig

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,6 +1,6 @@
 
 function ColorMyPencils(color)
-	color = color or "rose-pine"
+	color = color or "palenight"
 	vim.cmd.colorscheme(color)
 
 end
--- lua/theprimeagen/packer.lua
+++ lua/theprimeagen/packer.lua
@@ -11,23 +11,17 @@ return require('packer').startup(function(use)
 	  'nvim-telescope/telescope.nvim', tag = '0.1.1',
 	  -- or                            , branch = '0.1.x',
 	  requires = { {'nvim-lua/plenary.nvim'} }
+  
   }
-
-  use({ 
-	  'rose-pine/neovim',
-	  as = 'rose-pine',
-	  config = function()
-		  vim.cmd('colorscheme rose-pine')
-	  end
-  })
 
   use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
   use('nvim-treesitter/playground')
   use('theprimeagen/harpoon')
   use('mbbill/undotree')
   use('tpope/vim-fugitive')
-
-    use {
+  use('vim-airline/vim-airline')
+  use('drewtempelmeyer/palenight.vim')
+  use {
 	  'VonHeikemen/lsp-zero.nvim',
 	  branch = 'v2.x',
 	  requires = {
